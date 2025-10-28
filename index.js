d4632d17e1b41fc496e95efc6a94640fb46fd6bb
Refinaldii <refinaldi9786@gmail.com>
2025-10-28 14:32:38 +0700

    Sinkronisasi database

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+db.sequelize.sync()
+  .then(() => {
+    console.log('Database synced successfully');
+  })
+  .catch(err => {
+    console.log('Error syncing database:', err);
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
