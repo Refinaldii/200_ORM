e5b5a310aaee3fd5c93bcdb9e4c9c68b2158bcae
Refinaldii <refinaldi9786@gmail.com>
2025-10-28 14:32:11 +0700

    Tambah middleware untuk JSON dan form

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
